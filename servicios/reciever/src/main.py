79d2bab9014b9081f2296aabb6614ca6f8b08902
Gonzalo <gbd1004@alu.ubu.es>
2023-03-17 16:42:51 +0100

    Memoria, simulador y reciever

--- servicios/reciever/src/main.py
+++ servicios/reciever/src/main.py
@@ -1,5 +1,8 @@
 import json
 import socket
+import sys
+import time
+import logging
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -10,25 +13,56 @@ ORG = "TFG"
 TOKEN = "u_nT6lvnTJEfY1xrcGF7E6ypuHKXDLoGOKXm580Q2pyFwNYv8CY_yFGUkCgjPep387EWuhE3p90EQaYFkW5Zww=="
 URL = "http://database:8086"
 
-if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
+MAX_RETRIES = 10
+
+
+def get_dbconn():
     client = influxdb_client.InfluxDBClient(
         url=URL,
         token=TOKEN,
         org=ORG
     )
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    
+
+    return client
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+
+    connected = False
+    retries = 0
+
+    client = get_dbconn()
+
+    while not connected and retries < MAX_RETRIES:
+        try:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            connected = True
+        except:
+            retries += 1
+            time.sleep(0.5)
+
+    if not connected:
+        logging.critical("Fallo con la conexión a la base de datos")
+        sys.exit()
+
+    logging.info("Conexión a la base de datos realizada con éxito. Reintentos: " + str(retries))
+
     while True:
-        data, addr = sock.recvfrom(1024)       
+        data, addr = sock.recvfrom(1024)
         data_json = json.loads(data)
 
         point = influxdb_client.Point("agv_logs") \
-                    .tag("vid", str(data_json["id"])) \
-                    .field("bateria", int(data_json["bateria"])) \
-                    .field("velocidad", int(data_json["velocidad"])) \
-                    .field("estacion", int(data_json["estacion"])) \
-                    .time(data_json["tiempo"])
-        
+            .tag("vid", str(data_json["id"])) \
+            .field("bateria", int(data_json["bateria"])) \
+            .field("velocidad", int(data_json["velocidad"])) \
+            .field("punto", data_json["punto"]) \
+            .field("siguiente_punto", data_json["siguiente_punto"]) \
+            .field("pos_x", int(data_json["pos_x"])) \
+            .field("pos_y", int(data_json["pos_y"])) \
+            .time(data_json["tiempo"])
+
         write_api.write(bucket=BUCKET, org=ORG, record=point)
--- servicios/simulator/src/main.py
+++ servicios/simulator/src/main.py
@@ -5,10 +5,96 @@ import socket
 import random
 import json
 import time
+import itertools
 
 UDP_IP = "reciever"
 UDP_PORT = 5004
 
+
+class Punto:
+    nueva_id = itertools.count()
+
+    def __init__(self, x, y):
+        self.id = next(Punto.nueva_id)
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "[{0}: x:{1}, y:{2}]".format(self.id, self.x, self.y)
+
+
+class Mapa:
+    def __init__(self, lista_puntos: list()):
+        self.lista_puntos = lista_puntos
+
+
+class AGV:
+    nueva_id = itertools.count()
+
+    def __init__(self, punto_inicial: Punto, siguiente: Punto):
+        self.id = next(AGV.nueva_id)
+        self.bateria = 100.0
+        self.velocidad = 0.05
+        self.punto = punto_inicial
+        self.siguiente_punto = siguiente
+        self.pos_x = punto_inicial.x
+        self.pos_y = punto_inicial.y
+
+    def log(self, sock):
+        datos = {
+            "tiempo": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "id": self.id,
+            "bateria": self.bateria,
+            "velocidad": self.velocidad,
+            "punto": str(self.punto),
+            "siguiente_punto": str(self.siguiente_punto),
+            "pos_x": self.pos_x,
+            "pos_y": self.pos_y,
+        }
+        print(datos)
+        sock.sendto(bytes(json.dumps(datos), encoding="utf-8"), (UDP_IP, UDP_PORT))
+
+    def actualizar_bateria(self, dt):
+        if self.bateria > 0:
+            self.bateria -= dt / 10
+        else:
+            self.bateria = 0
+
+    def actualizar_posicion(self, dt):
+        if self.siguiente_punto is not None and self.bateria > 0:
+            dir_x = self.siguiente_punto.x - self.punto.x
+            dir_y = self.siguiente_punto.y - self.punto.y
+
+            self.pos_x = self.pos_x + dir_x * self.velocidad * dt
+            self.pos_y = self.pos_y + dir_y * self.velocidad * dt
+
+            if abs(self.pos_x - self.siguiente_punto.x) < 0.001 and \
+               abs(self.pos_y - self.siguiente_punto.y) < 0.001:
+                self.punto = self.siguiente_punto
+                self.pos_x = self.siguiente_punto.x
+                self.pos_y = self.siguiente_punto.y
+                self.siguiente_punto = None
+
+    def simular(self, sock):
+        dt = 0
+        tiempo_log = 5
+
+        while True:
+            start = time.time()
+            tiempo_log -= dt
+
+            self.actualizar_bateria(dt)
+            self.actualizar_posicion(dt)
+
+            if tiempo_log < 0:
+                self.log(sock)
+                tiempo_log = 5
+
+            end = time.time()
+            dt = end - start
+
+
+# Funcion temporal para generar datos
 def generar_datos():
     """Genera datos aleatorios del AGV con id=1"""
     dato = {
@@ -16,19 +102,26 @@ def generar_datos():
         "id": 1,
         "bateria": random.randint(0, 100),
         "velocidad": random.randint(0, 5),
-        "estacion": random.randint(0, 10)
+        "punto": str(Punto(random.randint(0, 5), random.randint(0,5))),
+        "siguiente_punto": str(Punto(random.randint(0, 5), random.randint(0,5))),
+        "pos_x": random.random() * 5,
+        "pos_y": random.random() * 5,
 
     }
 
     return json.dumps(dato)
 
+
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    while True:
-        datos = generar_datos()
-        sock.sendto(bytes(datos, encoding="utf-8"), (UDP_IP, UDP_PORT))
-        print(datos)
+    puntos = [
+        Punto(0, 0),
+        Punto(1, 0),
+        Punto(0, 1),
+        Punto(1, 1)
+    ]
+    mapa = Mapa(puntos)
+    agv = AGV(puntos[0], puntos[1])
 
-        time.sleep(5
-)
+    agv.simular(sock)
