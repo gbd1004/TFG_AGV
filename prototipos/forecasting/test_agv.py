bf1de02014d40db447a5d5c14e26d4ef3fdb6214
Gonzalo <gbd1004@alu.ubu.es>
2023-06-26 01:32:53 +0200

    Cambios prototipos

--- prototipos/forecasting/test_agv.py
+++ prototipos/forecasting/test_agv.py
@@ -1,3 +1,4 @@
+import logging
 from pandas import DataFrame
 from influxdb_client import InfluxDBClient, Point, WriteOptions
 import time
@@ -29,13 +30,14 @@ from darts.models.forecasting.linear_regression_model import LinearRegressionMod
 from darts.models.forecasting.random_forest import RandomForest
 from darts.models.forecasting.fft import FFT
 from darts.utils.statistics import plot_acf, plot_pacf
-from darts.metrics import mape
+from darts.metrics.metrics import mase
 
 import matplotlib.pyplot as plt
 import torch
 
 VAL_SIZE = 320
 
+logging.basicConfig(level=logging.INFO)
 torch.set_float32_matmul_precision('high')
 
 token = "u_nT6lvnTJEfY1xrcGF7E6ypuHKXDLoGOKXm580Q2pyFwNYv8CY_yFGUkCgjPep387EWuhE3p90EQaYFkW5Zww=="
@@ -45,14 +47,17 @@ client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
 query_api = client.query_api()
 
 def query_dataframe() -> DataFrame:
+        # ' |> range(start:2023-05-30, stop:2023-05-31)' \
+        # ' |> filter(fn: (r) => r._measurement == "test" and r.type == "value")' \
     query = 'from(bucket:"AGV")' \
-        ' |> range(start:2023-05-30, stop:2023-05-31)' \
+        ' |> range(start:-300s)' \
+        ' |> filter(fn: (r) => r._measurement == "AGVDATA" and r.type == "value")' \
         ' |> aggregateWindow(every: 200ms, fn: last, createEmpty: false)' \
-        ' |> filter(fn: (r) => r._measurement == "test" and r.type == "value")' \
         ' |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")'
 
     df = query_api.query_data_frame(query=query)
     df.drop(columns=['result', 'table', '_start', '_stop'])
+    df.drop(df.tail(1).index, inplace=True)
 
     df['_time'] = df['_time'].values.astype('<M8[us]')
     df.head()
@@ -69,14 +74,16 @@ df = query_dataframe()
 series_ed = get_series('encoder_derecho', df)
 
 
-# plot_acf(series_ed, max_lag=100)
-# plt.show()
-
 series_ed.plot()
 plt.show()
 
 series_ed = fill_missing_values(Diff(lags=5, dropna=False).fit_transform(series=series_ed))
 series_ed.plot()
+plt.show()
+
+plot_acf(series_ed, max_lag=20)
+plt.show()
+plot_pacf(series_ed, max_lag=20)
 plt.show()
 
 series_ei = get_series('encoder_izquierdo', df)
@@ -117,26 +124,27 @@ out = 300
 # model = KalmanForecaster(dim_x=1000)
 # model = Prophet()
 # model = TransformerModel(
-#     input_chunk_length=40,
-#     output_chunk_length=10,
-#     d_model=16,
+#     input_chunk_length=55,
+#     output_chunk_length=50,
+#     d_model=64,
 #     nhead=2,
 #     num_encoder_layers=4,
-#     num_decoder_layers=1,
+#     num_decoder_layers=2,
 #     dim_feedforward=512,
-#     dropout=0.48631234885236607,
-#     activation="SwiGLU",
-#     norm_type="LayerNorm"
+#     dropout=0.0616,
+#     activation="Bilinear",
+#     norm_type="RMSNorm"
 # )
-# model = TransformerModel(input_chunk_length=40, output_chunk_length=20)
+model = TransformerModel(input_chunk_length=60, output_chunk_length=50)
 # model = TFTModel(input_chunk_length=360, output_chunk_length=20)
 # model = NBEATSModel(input_chunk_length=100, output_chunk_length=out)
 # model = FFT()
-model = NHiTSModel(input_chunk_length=60, output_chunk_length=10)
+# model = NHiTSModel(input_chunk_length=60, output_chunk_length=50)
+# model = NHiTSModel(input_chunk_length=100, output_chunk_length=50, num_stacks=5, num_blocks=2, num_layers=1, layer_widths=11, dropout=0.4293, activation="Softplus", MaxPool1d=False)
 # model = RNNModel(input_chunk_length=100, model="LSTM")
-# model = TCNModel(input_chunk_length=40, output_chunk_length=10)
+# model = TCNModel(input_chunk_length=349, output_chunk_length=50, kernel_size=2, num_filters=17, weight_norm=False, dilation_base=7, num_layers=6, dropout=0.1288)
 # model = ARIMA()
-# model = ARIMA(p=15, d=0, q=10)
+# model = ARIMA(p=23, d=1, q=0)
 # model = AutoARIMA()
 # model = LinearRegressionModel(lags=10, lags_past_covariates=10, output_chunk_length=20)
 # model = RandomForest(lags=100, lags_past_covariates=100, output_chunk_length=50, max_depth=50)
@@ -145,28 +153,31 @@ model = NHiTSModel(input_chunk_length=60, output_chunk_length=10)
 # model.fit([train_ed, train_ei, train_sr, train_sl], epochs=20)
 # model.fit([train_ed, train_ei], past_covariates=[train_sr, train_sl], epochs=100)
 
-covariates = train_sr.stack(train_ei).stack(train_sl)
+
 multivariate_scaler = Scaler()
 multivariate_series = series_ed.stack(series_ei).stack(series_sl).stack(series_sr)
 multivariate_train, multivariate_val = multivariate_series[:-VAL_SIZE], multivariate_series[-VAL_SIZE:]
 multivariate_train_scaled = multivariate_scaler.fit_transform(multivariate_train).astype(np.float32)
 multivariate_val_scaled = multivariate_scaler.transform(multivariate_val).astype(np.float32)
-# model.fit(train_ed, past_covariates=covariates)
+
+covariates = train_sr_scaled.stack(train_sl_scaled)
+
+model.fit([train_ed_scaled, train_ei_scaled], past_covariates=[covariates, covariates])
 # model.fit(multivariate_train)
-model.fit(multivariate_train, val_series=multivariate_val, epochs=200)
+# model.fit(multivariate_train, val_series=multivariate_val, epochs=200)
 # model.fit(train_ed)
 # model.fit(train_scaled)
-prediction = model.predict(series=multivariate_train, n=40)
-# prediction = model.predict(n=40)
+# prediction = model.predict(series=multivariate_train, n=50)
+# prediction = model.predict(n=80)
 # prediction = model.predict(series=train_scaled, n=out)
-# prediction = model.predict(series=train_ed, past_covariates=covariates, n=50)
+prediction = model.predict(series=[train_ed_scaled, train_ei_scaled], past_covariates=[covariates, covariates], n=50)
 # prediction = model.predict(n=out)
 
 # print(prediction)
 
-# pred = scaler_ed.inverse_transform(prediction)
+pred = scaler_ed.inverse_transform(prediction[0])
 # pred = multivariate_scaler.inverse_transform(prediction)
-pred = prediction
+# pred = prediction
 
 # print(pred)
 
@@ -187,4 +198,8 @@ model.save("test_model.pt")
 # multivariate_series.plot()
 # historical.plot()
 # plt.show()
-# print("MAPE = {:.2f}%".format(mape(historical, series)))
+
+pred["encoder_derecho"].plot()
+plt.show()
+
+print("MASE = {}".format(mase(actual_series=val_ed, pred_series=pred, insample=train_ed)))
