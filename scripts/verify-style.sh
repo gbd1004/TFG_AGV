70eef1103e3ff1f4c4256f49b5e984e9c9686b3c
Gonzalo <gbd1004@alu.ubu.es>
2023-06-27 16:09:20 +0200

    Cambio script

--- scripts/verify-style.sh
+++ scripts/verify-style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pylint --fail-under=7.0 --recursive=y -f text services/forecasting_cpu services/forecasting_gpu services/reciever services/simulator
+pylint --fail-under=7.0 --recursive=y -f text services
